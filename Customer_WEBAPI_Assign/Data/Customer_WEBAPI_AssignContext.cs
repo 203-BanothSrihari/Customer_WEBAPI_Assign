adad2aab942d47c25485c4a5cf6e0e75825b77b6
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-20 14:41:54 +0530

    Add project files.

---
+++ Customer_WEBAPI_Assign/Data/Customer_WEBAPI_AssignContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Customer_WEBAPI_Assign.Model;
+
+namespace Customer_WEBAPI_Assign.Data
+{
+    public class Customer_WEBAPI_AssignContext : DbContext
+    {
+        public Customer_WEBAPI_AssignContext (DbContextOptions<Customer_WEBAPI_AssignContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer_WEBAPI_Assign.Model.Customer> Customer { get; set; }
+    }
+}
